eb2c6441a7fdc08eb368a58fe4961322e15d6449
Alex <lrl.msk@yandex.ru>
2022-04-21 07:51:07 +0300

    add zadacha2

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,12 @@
+﻿int number = new Random().Next(10, 1000);
+int number2 = new Random().Next(2, 5);
+if(number % number2 == 0)
+    {
+        System.Console.WriteLine($"Число {number} кратно числу {number2}");
+    }
+else
+{
+        System.Console.WriteLine($"Число {number} не кратно числу {number2}");
+        System.Console.WriteLine($"остаток от деления {number % number2}");
+}
+
