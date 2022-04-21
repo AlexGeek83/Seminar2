35092c0dbeec3b7c780cd5f3d46ac762f67fbcb3
Alex <lrl.msk@yandex.ru>
2022-04-21 08:36:56 +0300

    add zadacha3_1 с оператором или

---
+++ Zadacha3_1/Program.cs
@@ -0,0 +1,10 @@
+﻿int number = 25;
+int number2 = 7;
+int number3 = 23;
+
+if(number % number2 == 0 || number % number3 == 0) // оператор "ИЛИ" ||  
+    Console.WriteLine("Верно!");
+else
+System.Console.WriteLine("НЕ ВЕРНО!");
+
+//Выполняется если хотя бы ОДНО значение выполняется, если оба нет то и ничего не выполнено, т.е. НЕ ВЕРНО
