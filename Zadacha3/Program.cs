6609f27c34b520a95bd632b44e9c606cb322ee8b
Alex <lrl.msk@yandex.ru>
2022-04-21 08:21:03 +0300

    add zadacha3

---
+++ Zadacha3/Program.cs
@@ -0,0 +1,20 @@
+﻿int number = 161;
+int number2 = 7;
+int number3 = 23;
+
+if(number % number2 == 0 && number % number3 == 0) // && - проверяются сразу два условия - оператор "И"
+   // {
+   // if(number % number3 == 00)
+   // System.Console.WriteLine("Да кратны");
+   // else System.Console.WriteLine("Нет что то не кратно");
+   // }
+    {
+        System.Console.WriteLine($"Число {number} кратно числу {number2} и числу {number3}");
+    }
+    //else System.Console.WriteLine("Нет не кратно");
+else
+{
+        System.Console.WriteLine($"Число {number} не кратно числу {number2} и/или числу {number3}");
+    
+}
+
