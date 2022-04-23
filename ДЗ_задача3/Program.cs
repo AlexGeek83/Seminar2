1282c0107eb1358264721e350b67ae12d5679c8a
Alex <lrl.msk@yandex.ru>
2022-04-23 12:13:15 +0300

    add дз задача3

---
+++ ДЗ_задача3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 3: Напишите программу, которая выводит третью
+// цифру заданного числа или сообщает, что третьей цифры
+// нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+int number = 78;
+
+if(number > 100 && number < 1000)
+System.Console.WriteLine($"число {number} 3-х значное и 3-е число = {number % 10}");  
+
+if(number / 100 > 0)
+System.Console.WriteLine($"Третьей цифрой числа {number} является цифра {number % 1000 / 100}");
+else System.Console.WriteLine($"У числа {number} нет третьей цифры");
