5be74eb90abfc2294ff58bd5c67956ca837223b4
Alex <lrl.msk@yandex.ru>
2022-04-24 14:30:56 +0300

    add разбор дз задача1

---
+++ ДЗ задача1_разбор/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти вторую цифру справа
+// 123456 -> 2
+
+int num = 4258;
+if(num <= 9)
+{
+    System.Console.WriteLine("Нет второй цифры");
+}
+else
+{
+    while (num >= 100)
+    {
+    num /=10;
+    }
+    Console.WriteLine($"Вторая цифра справа числа {num} будет {num % 10} ");
+}
+
