f5ee4e37da607f37ef9a0f2ad75b954dca673371
Alex <lrl.msk@yandex.ru>
2022-04-24 14:49:26 +0300

    add разбор дз задача1 правки

--- ДЗ задача1_разбор/Program.cs
+++ ДЗ задача1_разбор/Program.cs
@@ -1,7 +1,14 @@
 ﻿// Найти вторую цифру справа
 // 123456 -> 2
 
+//string number = Console.ReadLine();
+//if (number.Length <= 1)
+//    System.Console.WriteLine("Нет второй цифры!");
+//else
+//    System.Console.WriteLine(number[1]);
+
 int num = 4258;
+// num = Math.Abs(num);  // модуль числа
 if(num <= 9)
 {
     System.Console.WriteLine("Нет второй цифры");
